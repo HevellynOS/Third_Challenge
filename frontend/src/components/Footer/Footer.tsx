45b7b4a2f0ae9dd0a116102d5fb027abe055df58
Hevellyn Oshiro <oshirohevellyn@gmail.com>
2023-08-11 11:05:36 -0300

    feat: added footer

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,31 @@
+
+
+
+import Logo from '../Logo/Logo'
+import facebookIcon from '../../assets/images/facebook icon.png'
+import instagramIcon from '../../assets/images/instagram icon.png'
+import twitterIcon from '../../assets/images/twitter icon.png'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+        <Logo logo={false}/>
+      <nav className='container-nav-footer'>
+        <ul>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Delivery</a></li>
+            <li><a href="#">Help & Support</a></li>
+            <li><a href="#">T&C</a></li>
+        </ul>
+        <ul>
+            <li><a href=""><img src={facebookIcon} alt="" /></a></li>
+            <li><a href=""><img src={instagramIcon} alt="" /></a></li>
+            <li><a href=""><img src={twitterIcon} alt="" /></a></li>
+        </ul>
+      </nav>
+        <div>Contact: <a href="#">+91 1234567899</a></div>
+    </footer>
+  )
+}
+
+export default Footer
