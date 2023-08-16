08b641f43e84d15b96635f12f4d5796f252eed1b
Hevellyn Oshiro <oshirohevellyn@gmail.com>
2023-08-16 18:07:07 -0300

    fix: now im reciving data from api4back using graphql and axios

---
+++ backend/hooks/useFitMeData.tsx
@@ -0,0 +1,89 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+interface FitMeData {
+  objectId: string;
+  name: string;
+  rating: number;
+  deliveryTime: string;
+  isExpensive: boolean;
+  location: string;
+  topDishes: TopDishData[];
+}
+
+interface TopDishData {
+  name: string;
+  description: string;
+  image: string;
+  price: number;
+}
+
+interface UseFitMeDataResult {
+  fitMeData: FitMeData[] | null;
+  loading: boolean;
+  error: string | null;
+}
+
+const useFitMeData = (): UseFitMeDataResult => {
+  const [fitMeData, setFitMeData] = useState<FitMeData[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchData = async () => {
+    const url = "https://parseapi.back4app.com/graphql";
+    const headers = {
+      "X-Parse-Application-Id": "DSiIkHz2MVbCZutKS7abtgrRVsiLNNGcs0L7VsNL",
+      "X-Parse-Master-Key": "0cpnqkSUKVkIDlQrNxameA6OmjxmrA72tsUMqVG9",
+      "X-Parse-Client-Key": "zXOqJ2k44R6xQqqlpPuizAr3rs58RhHXfU7Aj20V",
+      "Content-Type": "application/json; charset=utf-8",
+    };
+
+    const query = `
+      query {
+        fitMes {
+          count
+          edges {
+            node {
+              objectId
+              name
+              rating
+              deliveryTime
+              isExpensive
+              location
+              topDishes {
+                ...AllDishes
+              }
+            }
+          }
+        }
+      }
+      
+      fragment AllDishes on Dish {
+        name
+        description
+        image
+        price
+      }
+    `;
+
+    try {
+      setLoading(true);
+      const response = await axios.post(url, { query }, { headers });
+      const responseData = response.data.data.fitMes.edges.map((edge: any) => edge.node);
+      setFitMeData(responseData || null);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError("An error occurred while fetching data.");
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return { fitMeData, loading, error };
+};
+
+export default useFitMeData;
--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const fetchData = axios.create({
-  baseURL: "https://parseapi.back4app.com/",
+  baseURL: "https://parseapi.back4app.com/graphql",
   headers: {
-    "X-Parse-Application-Id": "lrAPveloMl57TTby5U0S4rFPBrANkAhLUll8jFOh",
-    "X-Parse-REST-API-Key": "8aqUBWOjOplfA6lstntyYsYVkt3RzpVtb8qU5x08",
+    "X-Parse-Application-Id": "DSiIkHz2MVbCZutKS7abtgrRVsiLNNGcs0L7VsNL",
+    "X-Parse-Master-Key": "0cpnqkSUKVkIDlQrNxameA6OmjxmrA72tsUMqVG9",
+    "X-Parse-Client-Key": "zXOqJ2k44R6xQqqlpPuizAr3rs58RhHXfU7Aj20V",
     "Content-Type": "application/json; charset=utf-8", // Include the charset parameter
   },
 });
